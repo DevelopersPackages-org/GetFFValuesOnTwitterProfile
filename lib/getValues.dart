d2070e3cee272f2dba4c28eeb92f4c7557b5b692
halka9000stg <csta19097@g.nihon-u.ac.jp>
2021-11-21 22:02:28 +0900

    fix: null safe
    Signed-off-by: halka9000stg <csta19097@g.nihon-u.ac.jp>

--- lib/getValues.dart
+++ lib/getValues.dart
@@ -25,10 +25,14 @@ Map<String,int> getFFValues(String screenName) async {
   }
 
   // 取得したHTMLのボディをパースする。
-  final document = parse(response.body);
-  int valFollowing = int.parse(document.querySelectorAll(selectorFollowing).text);
-  int valFollowers = int.parse(document.querySelectorAll(selectorFollowers).text);
-  return {"Following": valFollowing, "Followers": valFollowers}
+  Document document = parse(response.body);
+  Element? elFollowing = document.querySelector(selectorFollowing);
+  Element? elFollowers = document.querySelector(selectorFollowers);
+  if(elBuy==null||elSell==null){
+    return {"NoSuchRowErr": -1.toDouble()}
+  }else{
+  return {"Following": int.parse(elFollowing!.text), "Followers": int.parse(elFollowers!.text)}
+    }
 }
 List<Map<String, int>> getFFValuesForIn(List<String> screenNames) async {
   return screenNames.map((String screenName)=> await getFFValues(screenName)).toList();
